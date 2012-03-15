75a10f05fc3910f26493f332a38ca73c33d18ccb
Andriy Polishchuk <andriy.s.polishchuk@gmail.com>
2012-03-15 11:07:13 +0200

    Refactored binding names, tuned powershell width, etc.

--- init.el
+++ init.el
@@ -1,90 +1,148 @@
+;| Maximize emacs
+(w32-send-sys-command #xf030)
+
 ;| Utils
-(defmacro λ (&rest args)
-  `(lambda ,(butlast args)
-     ,(car (last args))))
+(defmacro x2 (form)
+  `(progn ,form
+	  ,form))
 
-(defun @ (f xs)
-  (mapc f xs))
-
-(defun map (f xs)
-  (mapcar f xs))
-
-(defun zip-with (f xs ys)
-  (if (and xs ys)
-      (cons (funcall f (car xs) (car ys))
-	    (zip-with f (cdr xs) (cdr ys)))))
+(defun site-load (path)
+  (load (concat site-lisp-path path)))
 
 ;| General
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+(scroll-bar-mode   -1)
+(tool-bar-mode     -1)
 (blink-cursor-mode -1)
-(setq blink-matching-paren nil)
-(desktop-save-mode 1)
-(setq inhibit-splash-screen t)
+
+(setq blink-matching-paren  nil
+      inhibit-splash-screen t)
 
 ;| Global-Keybindings
 (define-key global-map [(control \\)] [?\u03BB])
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 ;| UTF-8
-(setq default-buffer-file-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8
+      locale-coding-system              'utf-8)
+
+(set-terminal-coding-system  'utf-8)
+(set-keyboard-coding-system  'utf-8)
 (set-selection-coding-system 'utf-8)
+
 (prefer-coding-system 'utf-8)
 
-;| Paths [p]
-(setq p:site-lisp "C:/Program Files (x86)/Emacs/site-lisp/"
-      p:prog      "S:/prog/"
-      p:sandbox   "S:/prog/sandbox/"
-      p:hs-proj   "S:/prog/lang.haskell/-.proj/")
+;| Paths
+(setq site-lisp-path "C:/Program Files (x86)/Emacs/site-lisp/"
+      prog-path      "S:/prog/"
+      sandbox-path   "S:/prog/sandbox/"
+      hs-proj-path   "S:/prog/lang.haskell/-.proj/")
 
-(@ (λ p (add-to-list 'load-path (concat p:site-lisp p)))
-   '(""
-     "color-theme-6.6.0"
-     "slime-2011-11-09"))
+(mapc (lambda (path)
+	(add-to-list 'load-path
+		     (concat site-lisp-path path)))
+      '(""
+	"color-theme-6.6.0"
+	"slime-2011-11-09"))
 
-;| Windows [w]
-(setq w:code (car (get-buffer-window-list)))
+;| Code-Window Init
+(setq code-window (car (get-buffer-window-list)))
 
-;| Speedbar [w]
+;| Speedbar
 (require 'sr-speedbar)
 
-(@ 'speedbar-add-supported-extension
-   '(".hs" ".lhs" ".lisp"))
+(mapc 'speedbar-add-supported-extension
+      '(".hs" ".lhs" ".lisp"))
 
 (sr-speedbar-toggle)
 
-(setq w:sbar sr-speedbar-window)
-
 (defun speedbar-set-directory (path)
-  (select-window w:sbar)
+  (select-window sr-speedbar-window)
   (setq default-directory path)
   (speedbar-update-contents))
 
-;| Windows [w]
-(setq w:repl (split-window w:sbar 15)
-      w:menu (split-window w:sbar () t))
+;| Windows
+(setq interactive-window      (split-window sr-speedbar-window 15)
+      speedbar-menu-window    (split-window sr-speedbar-window () t)
+      interactive-menu-window (split-window speedbar-menu-window))
+
+;| PowerShell
+(autoload 'powershell "powershell"
+  "Run powershell as a shell within emacs." t)
+
+(setq powershell-process
+      (get-buffer-process (powershell)))
+
+; speedbar-menu-window resizer (to 13 columns)
+(push (lambda (_)
+	(let ((w (window-width speedbar-menu-window)))
+	  (when (> w 13)
+	    (with-selected-window speedbar-menu-window
+	      (shrink-window-horizontally (- w 13))))))
+      window-size-change-functions)
+
+; powershell-window resizer
+(push (lambda (_)
+	(if (= 59 (window-width interactive-window))
+	    (powershell--set-window-width powershell-process 59)))
+      window-size-change-functions)
 
 ;| Navigation
-(set-window-buffer w:menu "*scratch*")
-
 (require 'button)
 
-(with-current-buffer "*scratch*"
+(set-window-buffer speedbar-menu-window
+		   (generate-new-buffer "menu-a"))
+
+(defface green-link '((t (:foreground "green")))
+  "Face for green links in upper navigation menu."
+  :group 'basic-faces)
+
+(with-current-buffer "menu-a"
   (end-of-buffer)
-  (macrolet
-      (($ (s p)
-	  `(progn
-	     (insert-button
-	      ,s 'action (λ _ (speedbar-set-directory ,p)))
-	     (newline)
-	     (newline))))
-    ($ "hs-proj"   p:hs-proj)
-    ($ "sandbox"   p:sandbox)
-    ($ "prog"      p:prog)
-    ($ "site-lisp" p:site-lisp)))
+  (flet (($ (label path)
+	    (x2 (newline))
+	    (insert-button label
+			   'action `(lambda (_)
+				      (speedbar-set-directory ,path))
+			   'face 'green-link
+			   'help-echo nil)))
+    ($ " [ hs-proj ] "  hs-proj-path)
+    ($ " [ sandbox ] "  sandbox-path)
+    ($ "  [ prog ]   "  prog-path)
+    ($ "[ site-lisp ]"  site-lisp-path)))
+
+(set-window-buffer interactive-menu-window
+		   (generate-new-buffer "menu-b"))
+
+(defface yellow-link '((t (:foreground "yellow")))
+  "Face for yellow links in lower navigation menu."
+  :group 'basic-faces)
+
+(defface orange-link '((t (:foreground "orange")))
+  "Face for orange links in lower navigation menu."
+  :group 'basic-faces)
+
+(with-current-buffer "menu-b"
+  (end-of-buffer)
+  (flet (($ (label buffer &optional face action)
+	    (x2 (newline))
+	    (insert-button label
+			   'action (or action
+				       `(lambda (_)
+					  (set-window-buffer interactive-window
+							     ,buffer)))
+			   'face (or face
+				     'yellow-link)
+			   'help-echo nil)))
+    ($ "  [ ghci ]   " "*haskell*")
+    ($ "   [ ps ]    " "*PowerShell*")
+    (newline)
+    ($ " [ init.el ] " ()
+       'orange-link
+       (lambda (_)
+	 (with-selected-window code-window
+	   (set-window-buffer code-window
+			      (or (get-buffer "init.el")
+				  (find-file "~/.emacs.d/init.el"))))))))
 
 ;| Color-Theme
 (require 'color-theme)
@@ -92,18 +150,19 @@
 (color-theme-dark-blue2)
 
 ;| Haskell-Mode
-(load (concat p:site-lisp
-	      "haskell-mode-2.8.0/haskell-site-file.el"))
+(site-load "haskell-mode-2.8.0/haskell-site-file.el")
 
-(@ (λ e (add-hook 'haskell-mode-hook e))
-   '(turn-on-haskell-doc-mode
-     turn-on-haskell-indentation
-     font-lock-mode))
+(mapc (lambda (event)
+	(add-hook 'haskell-mode-hook event))
+      '(turn-on-haskell-doc-mode
+	turn-on-haskell-indentation
+	font-lock-mode))
 
 (require 'inf-haskell)
 
 (inferior-haskell-start-process '("ghci"))
-(set-window-buffer w:repl inferior-haskell-buffer)
+(set-window-buffer interactive-window
+		   inferior-haskell-buffer)
 
 ;| Octave
 (autoload 'octave-mode "octave-mod" nil t)
@@ -113,10 +172,11 @@
 	    auto-mode-alist))
 
 (add-hook 'octave-mode-hook
-          (λ (abbrev-mode 1)
-	     (auto-fill-mode 1)
-	     (if (eq window-system 'x)
-		 (font-lock-mode 1))))
+          (lambda ()
+	    (abbrev-mode 1)
+	    (auto-fill-mode 1)
+	    (if (eq window-system 'x)
+		(font-lock-mode 1))))
 
 ;| Slime
 (setq inferior-lisp-program "sbcl")
@@ -125,5 +185,5 @@
 (slime-setup '(slime-fancy slime-indentation))
 
 ;| Geiser
-(load-file (concat p:site-lisp "geiser-0.1.3/elisp/geiser.el"))
+(site-load "geiser-0.1.3/elisp/geiser.el")
 (setq geiser-impl-installed-implementations '(racket))
